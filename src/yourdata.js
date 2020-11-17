dfd601ef49fdeb7fdad76079efdbb11c1c0fa563
Adam <bbzdadem@gmail.com>
2020-11-18 01:14:27 +0400

    first app

---
+++ src/yourdata.js
@@ -0,0 +1,59 @@
+export default
+    {
+        
+        name :'bbzdadam.',
+        headerTagline: [
+                        'Hi 👋 You found me! I am Adam Baba.',
+                        'Web Developer ',
+                        'from Azerbaijan'
+    ],
+        contactEmail:'hello@adambaba.com',
+        abouttext: "Solution driven Web Developer adept at contributing to a highly collaborative work environment, finding solutions, and determining customer satisfaction. Proven experience developing consumer-focused websites using HTML, CSS, and JavaScript. Built products for desktop app users over 2+ years, meeting high standards for web design, user experience, best practices, and speed. Designed and developed web applications across multiple APIs, third-party integrations, and databases.",
+        aboutImage:'https://res.cloudinary.com/dgslkvls9/image/upload/v1605646199/IMG_3450s_bistgh.jpg',
+       ShowAboutImage:true,//true or false (Change Here)
+       projects:[
+           {
+            id: 1,
+            title:'OpexBot', 
+             service:'Web App', 
+            
+             imageSrc:"https://res.cloudinary.com/dgslkvls9/image/upload/v1605646570/ssssss_wy0k3u.png",
+             
+             url:'https://opexbot.com/'
+            },
+            {
+                id: 2,
+                title: 'MakeMyHw',
+                service: 'Website Development',
+                imageSrc: "https://res.cloudinary.com/dgslkvls9/image/upload/v1605646473/ssssss_ncnchj.png",
+                url: 'https://makemyhw.herokuapp.com/'
+            },
+            { 
+                id: 3,
+                title: 'Video Chat',
+                service: 'Web App',
+                imageSrc: "https://www.christianconnection.com/wp-content/uploads/2020/06/video-chat-blog.jpg",
+                url: 'https://videobotx.herokuapp.com/'
+            },
+        //     {
+        //         id: 4,
+        //         title: 'Project Four',
+        //         service: 'Branding',
+        //         imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        //         url: 'http://chetanverma.com/'
+        //    }
+        ],
+        social: [
+            {   name:'Github',
+                url:'https://github.com/bbzdadem'},
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/bbzdadam/'
+            },
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/bbzdadam/'
+            }
+
+        ]
+    }
